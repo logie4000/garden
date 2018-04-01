6317716d7aa56aa8614f8532beeaed5c0de12b52
Logie4000 <logie4000@gmail.com>
2018-04-01 10:12:58 -0400

    Update to build squares on create

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -1,2 +1,88 @@
 class BoxesController < ApplicationController
+  before_action :authorize, only: [:new, :create, :edit, :update]
+  before_action :authorize_as_admin, only: [:destroy]
+  before_action :set_box, only: [:show, :edit, :update, :destroy]
+
+  # GET /boxes
+  # GET /boxes.json
+  def index
+    @boxes = Box.all
+  end
+
+  # GET /boxes/1
+  # GET /boxes/1.json
+  def show
+  end
+
+  # GET /boxes/new
+  def new
+    @box = Box.new
+  end
+
+  # GET /boxes/1/edit
+  def edit
+  end
+
+  # POST /boxes
+  # POST /boxes.json
+  def create
+    @box = Box.new(box_params)
+
+    # Create squares for this box
+    if (params[:box][:type])
+      type = params[:box][:type]
+      if (type =~ /\dx\d/)
+        m = type.match(/(\d)x(\d)/)
+        rows = m[1]
+        cols = m[2]
+        rows.to_i.times do
+          cols.to_i.times do
+            @box.squares << Square.new(:row => , :column => )
+          end
+        end
+    respond_to do |format|
+      if @box.save
+        format.html { redirect_to @box, notice: 'Box was successfully created.' }
+        format.json { render :show, status: :created, location: @box }
+      else
+        format.html { render :new }
+        format.json { render json: @box.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /boxes/1
+  # PATCH/PUT /boxes/1.json
+  def update
+    respond_to do |format|
+      if @box.update(box_params)
+        format.html { redirect_to @box, notice: 'Box was successfully updated.' }
+        format.json { render :show, status: :ok, location: @box }
+      else
+        format.html { render :edit }
+        format.json { render json: @box.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /boxes/1
+  # DELETE /boxes/1.json
+  def destroy
+    @box.destroy
+    respond_to do |format|
+      format.html { redirect_to boxes_url, notice: 'Box was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_box
+      @box = Box.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def box_params
+      params.require(:box).permit(:label)
+    end
 end
